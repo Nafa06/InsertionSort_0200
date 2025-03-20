352fbfcbe0099588322542ba6efe552d73f95c4e
Nafa06 <naufalanggardi09@gmail.com>
2025-03-20 15:10:57 +0700

    Menambahkan fungsi insertionsort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -17,3 +17,15 @@ void input(){
 
 }
 
+void InsertionSort(){
+    int temp, j;
+    for (int i = 1; i < n; i++){
+        temp = arr{i};
+        j = i - 1;
+        while (j>= 0 && arr[j] > temp){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
